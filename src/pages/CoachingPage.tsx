48b73a022c3ab887b3ed00f4e5f4ae9666f46adb
2mendevs <2mendevs@gmail.com>
2026-05-10 16:14:18 +0530

    Reduce size of coaching card icons

--- src/pages/CoachingPage.tsx
+++ src/pages/CoachingPage.tsx
@@ -409,8 +409,8 @@ const css = `
   gap: 18px;
 }
  .cp-what-card-icon {
-  width: 80px;
-  height: 80px;
+  width: 70px;
+  height: 70px;
   display: flex;
   align-items: center;
   justify-content: center;
